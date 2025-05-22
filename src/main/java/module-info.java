8b81efef1aa45eefa061668295cdd6d1872ead54
brunomnsilva <brunomnsilva@gmail.com>
2025-05-22 16:18:19 +0100

    Bump version to 1.0.1. Java module.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * A fluent, type-safe CSS builder for JavaFX.
+ */
+module com.brunomnsilva.fluentfxcss {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.base;
+
+    exports com.brunomnsilva.fluentfxcss;
+    exports com.brunomnsilva.fluentfxcss.definitions;
+    exports com.brunomnsilva.fluentfxcss.stylers;
+    exports com.brunomnsilva.fluentfxcss.enums;
+    exports com.brunomnsilva.fluentfxcss.util;
+}
